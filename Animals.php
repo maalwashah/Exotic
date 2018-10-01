e2f12ef4c4b67086ce4181e4cc8b1d53c5893011
Monte <maalwashah@hawkmail.hfcc.edu>
2018-09-30 20:21:43 -0400

    Add files via upload

---
+++ Animals.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="big.css">
+    <title>yo</title>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,395 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="big.css">
+    <link rel="stylesheet" href="main.js">
+    <title>Hello, world!</title>
+
+  <script type="text/javascript">
+    $("document").ready(function(){
+      $("p").click(function(){
+        $("#p1").css("border","2px solid red");
+      });
+
+
+    });
+  </script>
+
+  </head>
+  <body>
+
+
+    <!-- Image and text -->
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Bootstrap
+      </a>
+    </nav>
+
+
+
+
+
+    <h1 class=".boobs">Hello, world!</h1>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+    <a class="navbar-brand" href="#"><strong>Guide Here</strong></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+
+  <div class="media">
+<img class="mr-3" src="images\blueMacaw.jpg" alt="Blue McCaw" width="365px" height="365px">
+<div class="media-body">
+  <h5 class="mt-0">Blue McCaw</h5>
+<p>  bluuuuu sit amet nibh libero, in gravida nulla. Nulla vel
+  metus scelerisque ante sollicitudin. Cras purus odio,
+  vestibulum in vulputate at, tempus viverra turpis.
+  Fusce condimentum nunc ac nisi vulputate fringilla.
+  Donec lacinia congue felis in faucibus.
+</p>
+
+<h5 class="mt-0">Facts</h5>
+<div class="row">
+  <div class="col-4">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active bg-info" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Description:</a>
+      <a class="list-group-item list-group-item-action bg-info" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Biology:</a>
+      <a class="list-group-item list-group-item-action bg-info" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Diet:</a>
+      <a class="list-group-item list-group-item-action bg-info" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Extinction:</a>
+    </div>
+  </div>
+  <div class="col-8">
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active " id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+        <p id="p1">
+          These birds can reach a length of 76–86 cm (30–34 in) and
+          weigh 0.900–1.5 kg (2–3 lb), making them some of the
+          larger members of their family. They are vivid in appearance
+          with blue-green wings and tail,
+          dark-blue chin, golden under parts, and a green forehead.
+        </p>
+      </div>
+      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+        <p id="p2">
+        Proportionately larger beaks,
+         long tails, and relatively bare, light-coloured,
+          medial (facial patch) areas distinguish macaws from
+          other parrots. Sometimes the facial patch is smaller
+          in some species and limited to </p> </div>
+      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+<p id="p3">Macaws eat a variety of foods including seeds, nuts,
+  fruits, palm fruits, leaves, flowers, and stems.
+   Wild species may forage widely</p>
+      </div>
+      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+        <p class="p4">The blue-and-yellow macaw generally mates for life.
+          They nest almost exclusively in dead palms and
+          most nests are in YemenVille. The male bird's color signals readiness for breeding.
+           The brighter and bolder the colors,
+          the better the chance of getting a mate</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+</div>
+
+<!---Second Animal -->
+
+<div class="media">
+<img class="mr-3" src="images\macaw.jpg" alt="Blue McCaw" width="365px" height="365px">
+<div class="media-body">
+<h5 class="mt-0">Blue McCaw</h5>
+<p>  bluuuuu sit amet nibh libero, in gravida nulla. Nulla vel
+metus scelerisque ante sollicitudin. Cras purus odio,
+vestibulum in vulputate at, tempus viverra turpis.
+Fusce condimentum nunc ac nisi vulputate fringilla.
+Donec lacinia congue felis in faucibus.
+</p>
+
+<h5 class="mt-0">Facts</h5>
+<div class="row">
+<div class="col-4">
+  <div class="list-group" id="list-tab" role="tablist">
+    <a class="list-group-item list-group-item-action active bg-info" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Description:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Biology:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Diet:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Extinction:</a>
+  </div>
+</div>
+<div class="col-8">
+  <div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active " id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+      <p id="p1">
+        These birds can reach a length of 76–86 cm (30–34 in) and
+        weigh 0.900–1.5 kg (2–3 lb), making them some of the
+        larger members of their family. They are vivid in appearance
+        with blue-green wings and tail,
+        dark-blue chin, golden under parts, and a green forehead.
+      </p>
+    </div>
+    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+      <p id="p2">
+      Proportionately larger beaks,
+       long tails, and relatively bare, light-coloured,
+        medial (facial patch) areas distinguish macaws from
+        other parrots. Sometimes the facial patch is smaller
+        in some species and limited to </p> </div>
+    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+<p id="p3">Macaws eat a variety of foods including seeds, nuts,
+fruits, palm fruits, leaves, flowers, and stems.
+ Wild species may forage widely</p>
+    </div>
+    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+      <p id="p4">The blue-and-yellow macaw generally mates for life.
+        They nest almost exclusively in dead palms and
+        most nests are in YemenVille. The male bird's color signals readiness for breeding.
+         The brighter and bolder the colors,
+        the better the chance of getting a mate</p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+</div>
+
+<!---third Animal -->
+
+<div class="media">
+<img class="mr-3" src="images\blueMacaw.jpg" alt="Blue McCaw" width="365px" height="365px">
+<div class="media-body">
+<h5 class="mt-0">Blue McCaw</h5>
+<p>  bluuuuu sit amet nibh libero, in gravida nulla. Nulla vel
+metus scelerisque ante sollicitudin. Cras purus odio,
+vestibulum in vulputate at, tempus viverra turpis.
+Fusce condimentum nunc ac nisi vulputate fringilla.
+Donec lacinia congue felis in faucibus.
+</p>
+
+<h5 class="mt-0">Facts</h5>
+<div class="row">
+<div class="col-4">
+  <div class="list-group" id="list-tab" role="tablist">
+    <a class="list-group-item list-group-item-action active bg-info" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Description:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Biology:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Diet:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Extinction:</a>
+  </div>
+</div>
+<div class="col-8">
+  <div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active " id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+      <p id="p1">
+        These birds can reach a length of 76–86 cm (30–34 in) and
+        weigh 0.900–1.5 kg (2–3 lb), making them some of the
+        larger members of their family. They are vivid in appearance
+        with blue-green wings and tail,
+        dark-blue chin, golden under parts, and a green forehead.
+      </p>
+    </div>
+    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+      <p id="p2">
+      Proportionately larger beaks,
+       long tails, and relatively bare, light-coloured,
+        medial (facial patch) areas distinguish macaws from
+        other parrots. Sometimes the facial patch is smaller
+        in some species and limited to </p> </div>
+    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+<p id="p3">Macaws eat a variety of foods including seeds, nuts,
+fruits, palm fruits, leaves, flowers, and stems.
+ Wild species may forage widely</p>
+    </div>
+    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+      <p id="p4">The blue-and-yellow macaw generally mates for life.
+        They nest almost exclusively in dead palms and
+        most nests are in YemenVille. The male bird's color signals readiness for breeding.
+         The brighter and bolder the colors,
+        the better the chance of getting a mate</p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+</div>
+
+<!---fourth Animal -->
+
+<div class="media">
+<img class="mr-3" src="images\blueMacaw.jpg" alt="Blue McCaw" width="365px" height="365px">
+<div class="media-body">
+<h5 class="mt-0">Blue McCaw</h5>
+<p>  bluuuuu sit amet nibh libero, in gravida nulla. Nulla vel
+metus scelerisque ante sollicitudin. Cras purus odio,
+vestibulum in vulputate at, tempus viverra turpis.
+Fusce condimentum nunc ac nisi vulputate fringilla.
+Donec lacinia congue felis in faucibus.
+</p>
+
+<h5 class="mt-0">Facts</h5>
+<div class="row">
+<div class="col-4">
+  <div class="list-group" id="list-tab" role="tablist">
+    <a class="list-group-item list-group-item-action active bg-info" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Description:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Biology:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Diet:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Extinction:</a>
+  </div>
+</div>
+<div class="col-8">
+  <div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active " id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+      <p id="p1">
+        These birds can reach a length of 76–86 cm (30–34 in) and
+        weigh 0.900–1.5 kg (2–3 lb), making them some of the
+        larger members of their family. They are vivid in appearance
+        with blue-green wings and tail,
+        dark-blue chin, golden under parts, and a green forehead.
+      </p>
+    </div>
+    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+      <p id="p2">
+      Proportionately larger beaks,
+       long tails, and relatively bare, light-coloured,
+        medial (facial patch) areas distinguish macaws from
+        other parrots. Sometimes the facial patch is smaller
+        in some species and limited to </p> </div>
+    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+<p id="p3">Macaws eat a variety of foods including seeds, nuts,
+fruits, palm fruits, leaves, flowers, and stems.
+ Wild species may forage widely</p>
+    </div>
+    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+      <p id="p4">The blue-and-yellow macaw generally mates for life.
+        They nest almost exclusively in dead palms and
+        most nests are in YemenVille. The male bird's color signals readiness for breeding.
+         The brighter and bolder the colors,
+        the better the chance of getting a mate</p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+</div>
+
+<!---fifth Animal -->
+
+<div class="media">
+<img class="mr-3" src="images\blueMacaw.jpg" alt="Blue McCaw" width="365px" height="365px">
+<div class="media-body">
+<h5 class="mt-0">Blue McCaw</h5>
+<p>  bluuuuu sit amet nibh libero, in gravida nulla. Nulla vel
+metus scelerisque ante sollicitudin. Cras purus odio,
+vestibulum in vulputate at, tempus viverra turpis.
+Fusce condimentum nunc ac nisi vulputate fringilla.
+Donec lacinia congue felis in faucibus.
+</p>
+
+<h5 class="mt-0">Facts</h5>
+<div class="row">
+<div class="col-4">
+  <div class="list-group" id="list-tab" role="tablist">
+    <a class="list-group-item list-group-item-action active bg-info" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Description:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Biology:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Diet:</a>
+    <a class="list-group-item list-group-item-action bg-info" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Extinction:</a>
+  </div>
+</div>
+<div class="col-8">
+  <div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active " id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+      <p id="p1">
+        These birds can reach a length of 76–86 cm (30–34 in) and
+        weigh 0.900–1.5 kg (2–3 lb), making them some of the
+        larger members of their family. They are vivid in appearance
+        with blue-green wings and tail,
+        dark-blue chin, golden under parts, and a green forehead.
+      </p>
+    </div>
+    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+      <p id="p2">
+      Proportionately larger beaks,
+       long tails, and relatively bare, light-coloured,
+        medial (facial patch) areas distinguish macaws from
+        other parrots. Sometimes the facial patch is smaller
+        in some species and limited to </p> </div>
+    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+<p id="p3">Macaws eat a variety of foods including seeds, nuts,
+fruits, palm fruits, leaves, flowers, and stems.
+ Wild species may forage widely</p>
+    </div>
+    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+      <p id="p4">The blue-and-yellow macaw generally mates for life.
+        They nest almost exclusively in dead palms and
+        most nests are in YemenVille. The male bird's color signals readiness for breeding.
+         The brighter and bolder the colors,
+        the better the chance of getting a mate</p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+</div>
+
+<!--End Animals -->
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
